68139f991e38d9fe65a85f28a9172ca0511232c1
Joel Coddington <coddingtonjoel@gmail.com>
2021-03-17 23:35:13 -0700

    Added Descartes Quote Animation

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const quote = CSSRulePlugin.getRule(".quote-one::after");
+// const quoteTwo = CSSRulePlugin.getRule(".quote-two:after");
+// const quoteThree = CSSRulePlugin.getRule(".quote-three:after");
+// const quoteFour = CSSRulePlugin.getRule(".quote-four:after");
 let tl = new TimelineMax();
 let controller = new ScrollMagic.Controller();
 
@@ -80,17 +84,131 @@ tl.to(".line", 1, {
 
 // quote silently moves to center
 tl.to(".quote-container", 0, {
-    top: "108%",
-    left: "53%",
+    top: "80%",
+    left: "18%",
     transform: "translate(-50%, -50%)",
 });
 
-// line shifts as if typing
-// tl.to(".line", 4);
+// hide author
+tl.to(".author", 0, { opacity: 0 });
+
+// hide lines 2-4 of quote
+tl.to(".quote-two", 0, { opacity: 0 });
+tl.to(".quote-three", 0, { opacity: 0 });
+tl.to(".quote-four", 0, { opacity: 0 });
+
 tl.to(".quote-container", 1, { opacity: 1 });
 
+// move line to starting place of text
+tl.to(".line", { x: "20px" });
+
+// shift ::after of line 1
+tl.to(quote, 5, {
+    transform: "translateX(565px)",
+});
+tl.to(
+    ".line",
+    5,
+    {
+        x: "590px",
+    },
+    "-=5"
+);
+
+// shift ::after of line 2
+tl.to(quote, 0, { transform: "translateX(0) translateY(85px)" });
+tl.to(".line", 0, {
+    x: "20px",
+    y: "85px",
+});
+tl.to(".quote-two", 0, { opacity: 1 });
+
+tl.to(quote, 5, {
+    transform: "translateX(825px) translateY(85px)",
+});
+tl.to(
+    ".line",
+    5,
+    {
+        x: "850px",
+        y: "85px",
+    },
+    "-=5"
+);
+
+// shift ::after of line 3
+tl.to(quote, 0, { transform: "translateX(0) translateY(175px)" });
+tl.to(".line", 0, {
+    x: "20px",
+    y: "175px",
+});
+tl.to(".quote-three", 0, { opacity: 1 });
+
+tl.to(quote, 5, {
+    transform: "translateX(1025px) translateY(175px)",
+});
+tl.to(
+    ".line",
+    5,
+    {
+        x: "1050px",
+        y: "175px",
+    },
+    "-=5"
+);
+
+// shift ::after of line 4
+tl.to(quote, 0, { transform: "translateX(0) translateY(260px)" });
+tl.to(".line", 0, {
+    x: "20px",
+    y: "260px",
+});
+tl.to(".quote-four", 0, { opacity: 1 });
+
+tl.to(quote, 5, {
+    transform: "translateX(565px) translateY(260px)",
+});
+tl.to(
+    ".line",
+    5,
+    {
+        x: "590px",
+        y: "260px",
+    },
+    "-=5"
+);
+
+// show author
+tl.to(".author", 2, { opacity: 1 });
+
+// line blinks
+tl.to(".line", 1, {
+    delay: 0.5,
+    opacity: 0,
+})
+    .to(".line", 1, {
+        delay: 0.5,
+        opacity: 0.2,
+    })
+    .to(".line", 1, {
+        delay: 0.5,
+        opacity: 0,
+    })
+    .to(".line", 1, {
+        delay: 0.5,
+        opacity: 0.2,
+    })
+    .to(".line", 1, {
+        delay: 0.5,
+        opacity: 0,
+    })
+    .to(".line", 1, {
+        delay: 0.5,
+        opacity: 0.2,
+    });
+
 let scene = new ScrollMagic.Scene({
-    duration: 4000,
+    duration: 8000,
     triggerHook: 0.5,
     triggerElement: ".scene",
 })
